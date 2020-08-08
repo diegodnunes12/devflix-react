93ee0194273b6815e57f76dadf67361c5544b625
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-08-07 21:02:03 -0300

    Formulario de cadastro de categorias

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,19 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault'
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
+
+    const [categorias, setCategoria] = useState([])
+    const [nomeCategoria, setNomeCategoria] = useState('filmes')
+
     return (
       <PageDefault>
-        <h1>Cadastro de categoria</h1>
+        <h1>Cadastro da categoria: {nomeCategoria}</h1>
 
-        <form>
-            <label>
+        <form onSubmit={function handleSubmit(e) {
+            e.preventDefault()
+            setCategoria([
+              ...categorias,
+              nomeCategoria
+            ])
+          }}
+        >
+            <div>
+              <label>
+                Nome:
+                <input type="text" value={nomeCategoria} onChange={function  pegarValue(e) {
+                    setNomeCategoria(e.target.value)
+                  }} 
+                />
+              </label>
+            </div>
+            <div>
+              <label>
               Descrição:
-              <input type="text" />
+              <textarea type="text" value={nomeCategoria} onChange={function  pegarValue(e) {
+                  setNomeCategoria(e.target.value)
+                }} 
+              />
             </label>
+            </div>
+            <div>    
+            <label>
+              Cor:
+              <input type="color" value={nomeCategoria} onChange={function  pegarValue(e) {
+                  setNomeCategoria(e.target.value)
+                }} 
+              />
+            </label>
+            </div>
             <button>Cadastrar</button>
         </form>
+
+        <h3 style={{marginTop:'40px'}}>Categorias Cadastradas</h3>
+        <ul>
+          {categorias.map( (categoria, indice) =>{
+            return <li key={`${categoria}${indice }`}>{categoria}</li>
+          })
+          }
+        </ul>
 
         <Link to="/">
             Ir para home
