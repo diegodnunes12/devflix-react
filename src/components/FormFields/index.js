6c5d11d2fda6954eab63d3ebf2bbdcf63e337a11
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-08-10 20:16:20 -0300

    Melhorando os formulários

--- src/components/FormFields/index.js
+++ src/components/FormFields/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import ProtoTypes from 'prop-types'
 
 function FormFields({label, type, name, valor, onChange}) {
+    const fieldId = `id_${name}`
+
     return (
         <div>
-            <label>
+            <label htmlFor={fieldId}>
             {label}:
-            <input type={type} name={name} value={valor} onChange={onChange} 
+            <input id={fieldId} type={type} name={name} value={valor} onChange={onChange} 
             />
             </label>
         </div>
     )
+}
+
+FormFields.defaultProps = {
+    type: 'text'
+}
+
+FormFields.protoTypes = {
+    label: ProtoTypes.string.isRequired, 
+    type: ProtoTypes.string,
+    /* name: ProtoTypes.string.isRequired,  
+    valor: ProtoTypes.string.isRequired, 
+    onChange: ProtoTypes.string.isRequired */
 }
 
 export default FormFields;
