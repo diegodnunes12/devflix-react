fdf8228a401931c20ee375a72b24559c8f2a2f08
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-08-24 20:18:53 -0300

    Criando o formulário de cadastro de vídeos

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import PageDefault from '../../../components/PageDefault'
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import FormFields from '../../../components/FormFields';
+import useForm from '../../../hooks/useForm';
+import Button from '../../../components/Button';
 
 function CadastroVideo() {
+  const history = useHistory()
+  const { handleChange, valores } = useForm({
+
+  })
+
     return (
       <PageDefault>
         <h1>Cadastro de video</h1>
+
+        <form onSubmit={(event) => {
+          event.preventDefault()
+          history.push('/')
+        }} >
+          <FormFields label="Titulo do vídeo" name="titulo" value={valores.titulo} onChange={handleChange} />
+
+          <Button type="submit">Cadastrar</Button>
+        </form>
+
         <Link to="/cadastro/categoria">
             Cadastrar Categoria
         </Link>
